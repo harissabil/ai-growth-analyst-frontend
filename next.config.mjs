b0ba8aa60359bc657d0ae9f5018e5fb643e0beb9
Haris <harissabil04@gmail.com>
2025-09-01 23:06:12 +0700

    Rename next.config.ts to next.config.mjs and update type annotations

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Optional but nice if you later deploy a minimal bundle
+    // output: 'standalone',
+};
+
+export default nextConfig;
